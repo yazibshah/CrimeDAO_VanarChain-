bf4e15ad84d0711522e5fff31fb6eb8163b842b4
YazibPrac <yazibshahnawaz@gmail.com>
2024-07-18 02:45:28 +0500

    updated code

--- Interface/IUserComplaint.sol
+++ Interface/IUserComplaint.sol
@@ -7,16 +7,16 @@ interface IUserComplaint {
         address userAddress;
         string description;
         uint256 amount;
-        bytes32[] ipfsHashes;
+        string[] uri;
         bool resolved;
     }
 
-    event ComplaintFiled(uint256 complaintId, address userAddress, string description, uint256 amount, bytes32[] ipfsHashes);
+    event ComplaintFiled(uint256 complaintId, address userAddress, string description, uint256 amount, string[] ipfsHashes);
     event EthReceived(address from, uint256 amount);
 
-    function fileComplaint(string memory description, bytes32[] memory ipfsHashes) external payable;
+    function fileComplaint(string memory description, string[] memory _uri) external payable;
+    function uploadFiles(string[] memory _uri,uint256 _ComplaintId ) external;
     function getComplaint(uint256 complaintId) external view returns (Complaint memory);
-    function decodeIpfsHash(bytes32 ipfsHash) external pure returns (string memory);
-     function balanceOf() external view returns (uint256);
+    function balanceOf() external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
 }
--- UserComplaint.sol
+++ UserComplaint.sol
@@ -19,53 +19,59 @@ contract UserComplaint is Ownable, IUserComplaint {
     uint256 public constant COMPLAINT_FEE = 0.01 ether;
     uint256 private constant TOKEN_UNIT = 10 ** 18;
 
-    constructor(address userRegistrationAddress, address tokenAddress) Ownable(msg.sender){
+    constructor(address userRegistrationAddress, address tokenAddress) Ownable(msg.sender) {
         userRegistration = UserRegistration(payable(userRegistrationAddress));
         crimeDAOToken = CrimeDAOToken(payable(tokenAddress));
     }
 
-    function fileComplaint(string memory description, bytes32[] memory ipfsHashes) external payable override onlyRegisteredUser  {
-        require(userRegistration.getUser(msg.sender).registered,"please registred your self");
+    function fileComplaint(string memory description, string[] memory _uri) external payable override onlyRegisteredUser {
+        require(userRegistration.getUser(msg.sender).registered, "Please register yourself");
         require(msg.value >= COMPLAINT_FEE, "Incorrect complaint fee");
-        uint256 returnedFee=msg.value-COMPLAINT_FEE;
-        console.log(returnedFee);
-        
-        if(returnedFee>0){
-        (bool extraFee, ) = payable(msg.sender).call{value: returnedFee}("");
-        require(extraFee,"Refund Fail");
-        console.log(extraFee);
 
+        uint256 returnedFee = msg.value - COMPLAINT_FEE;
+        if (returnedFee > 0) {
+            (bool extraFee, ) = payable(msg.sender).call{value: returnedFee}("");
+            require(extraFee, "Refund failed");
         }
 
-        
         (bool success, ) = payable(crimeDAOToken).call{value: COMPLAINT_FEE}("");
         require(success, "Failed to send ETH to CrimeDAOToken contract");
 
         _complaintIdCounter.increment();
         uint256 newComplaintId = _complaintIdCounter.current();
 
-        complaints[newComplaintId] = Complaint(newComplaintId, msg.sender, description, msg.value, ipfsHashes, false);
+        complaints[newComplaintId] = Complaint(newComplaintId, msg.sender, description, msg.value, _uri, false);
 
-        // Mint and transfer CrimeDAO tokens
-        
-
-        emit ComplaintFiled(newComplaintId, msg.sender, description, msg.value, ipfsHashes);
+        emit ComplaintFiled(newComplaintId, msg.sender, description, msg.value, _uri);
     }
 
+    function uploadFiles(string[] memory _uri, uint256 _complaintId) external {
+    require(msg.sender == complaints[_complaintId].userAddress, "You are not the complaint owner");
+    Complaint storage complaint = complaints[_complaintId];
+    uint256 length = _uri.length;
+    for (uint256 i = 0; i < length; i++) {
+        complaint.uri.push(_uri[i]);
+    }
+}
+
+
     function getComplaint(uint256 complaintId) external view override returns (Complaint memory) {
-        require(complaints[complaintId].userAddress == msg.sender || owner() == msg.sender,"Not authorized");
+        require(complaints[complaintId].userAddress == msg.sender || owner() == msg.sender, "Not authorized");
         return complaints[complaintId];
     }
 
-    function decodeIpfsHash(bytes32 ipfsHash) external pure  returns (string memory) {
-        bytes memory alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-        bytes memory encoded = new bytes(46);
-        encoded[0] = alphabet[18];
-        encoded[1] = alphabet[27];
-        for (uint256 i = 0; i < 32; i++) {
-            encoded[i + 2] = alphabet[uint8(ipfsHash[i])];
-        }
-        return string(encoded);
+
+    function balanceOf() external view override onlyOwner returns (uint256) {
+        return crimeDAOToken.balanceOf(address(this));
+    }
+
+    function transfer(address recipient, uint256 amount) external override onlyOwner returns (bool) {
+        require(recipient != address(0), "Invalid recipient address");
+        require(amount > 0, "Amount must be greater than zero");
+        uint256 _amount = amount * TOKEN_UNIT;
+        bool success = crimeDAOToken.transfer(recipient, _amount);
+        require(success, "Token transfer failed");
+        return success;
     }
 
     modifier onlyRegisteredUser() {
@@ -73,27 +79,10 @@ contract UserComplaint is Ownable, IUserComplaint {
         _;
     }
 
-
-    
-    function balanceOf() external view onlyOwner returns (uint256){
-         return crimeDAOToken.balanceOf(address(this));
-    }
-
-    function transfer(address recipient, uint256 amount) external onlyOwner returns (bool){
-        require(recipient != address(0), "Invalid recipient address");
-        require(amount > 0, "Amount must be greater than zero");
-        uint _amount=amount*TOKEN_UNIT;
-        bool success = crimeDAOToken.transfer(recipient,_amount);
-        require(success, "Token transfer failed");
-        
-        return success;
-    }
-
-    // Fallback function to receive ETH
     receive() external payable {
-        require(msg.value>0,"Insufficient Fund");
-        (bool EthTranOwner,)=payable(owner()).call{value:msg.value}("");
-        require(EthTranOwner,"Transaction Fail");
+        require(msg.value > 0, "Insufficient fund");
+        (bool ethTranOwner, ) = payable(owner()).call{value: msg.value}("");
+        require(ethTranOwner, "Transaction failed");
         emit EthReceived(msg.sender, msg.value);
     }
 }
